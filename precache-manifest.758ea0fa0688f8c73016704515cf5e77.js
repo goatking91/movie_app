cf0d8228f67a630a5e1118fb48f3f0deaeed24e1
염승민 <tmdals3021@gmail.com>
2019-02-09 13:15:06 +0900

    Updates

---
+++ precache-manifest.758ea0fa0688f8c73016704515cf5e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "89f39d4c633e9351e5b4",
+    "url": "/movie_app/static/js/main.89f39d4c.chunk.js"
+  },
+  {
+    "revision": "da87d06f04d863a24d6d",
+    "url": "/movie_app/static/js/1.da87d06f.chunk.js"
+  },
+  {
+    "revision": "89f39d4c633e9351e5b4",
+    "url": "/movie_app/static/css/main.d600e4b0.chunk.css"
+  },
+  {
+    "revision": "afe58a8eaddc2edd6b0965834f22adbd",
+    "url": "/movie_app/index.html"
+  }
+];
